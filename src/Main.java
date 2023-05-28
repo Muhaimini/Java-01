b78f166a5fb9dd0f1232aae46f076c8a53bb856a
Muhaimini <muhaimin432@gmail.com>
2023-05-29 03:44:18 +0700

    fix: setter and getter

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 interface  MyClassInterface {
     public void myFirstNumber (int number);
     public void displayData();
@@ -11,7 +13,6 @@ class Myclass implements  MyClassInterface {
         this.isValidUser = initial;
     }
 
-
     public void myFirstNumber(int number){
         System.out.println("My first number is " + number);
     }
@@ -21,14 +22,36 @@ class Myclass implements  MyClassInterface {
     }
 }
 
+class ScannerClass {
+    public void runScanner() {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = myScanner.nextLine();
+
+        System.out.println("Result: " + text);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         int first_number = 100;
         boolean validUser = false;
 
         Myclass myClass = new Myclass(validUser);
+        ScannerClass scanner = new ScannerClass();
+
+        // from other file
+        Profile profile = new Profile();
+
+        profile.setMyName("Muhaimin");
+        profile.setMyAge(27);
+
+        System.out.println("My name is: "+ profile.getMyName());
+        System.out.println("Age: " + profile.getMyAge());
 
         myClass.displayData();
         myClass.myFirstNumber(first_number);
+        scanner.runScanner();
     }
 }
---
+++ src/Profile.java
@@ -0,0 +1,20 @@
+public class Profile {
+    private String myName;
+    private int age;
+
+    public void setMyName(String name){
+        this.myName = name;
+    }
+
+    public void setMyAge(int age){
+        this.age = age;
+    }
+
+    public String getMyName(){
+        return myName;
+    }
+
+    public int getMyAge(){
+        return age;
+    }
+}
